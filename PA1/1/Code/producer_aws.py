bdac0149fc3bf29fe7cdf11cb79c2edb46b7d67b
HAO-FU-1998 <hao.fu@vanderbilt.edu>
2022-08-28 13:18:00 -0500

    Add files via upload

---
+++ PA1/1/Code/producer_aws.py
@@ -0,0 +1,57 @@
+import os   # need this for popen
+import time # for sleep
+import json
+from kafka import KafkaProducer  # producer of events
+from datetime import datetime
+import urllib.request as r
+
+# We can make this more sophisticated/elegant but for now it is just
+# hardcoded to the setup I have on my local VMs
+
+# acquire the producer
+# (you will need to change this to your bootstrap server's IP addr)
+producer = KafkaProducer (bootstrap_servers="54.242.145.238:9092",
+                          acks=1, api_version=(0,10,1),
+                          value_serializer=lambda v: json.dumps(v).encode('utf-8')
+                                          )  # wait for leader to write to log
+
+# say we send the contents 100 times after a sleep of 1 sec in between
+for i in range (100):
+
+    # get the output of the top command
+    #process = os.popen ("top -n 1 -b")
+
+    # read the contents that we wish to send as topic content
+    #contents = process.read ()
+    # contents = str("test")
+
+    url='http://api.openweathermap.org/data/2.5/weather?q=chongqing&mode=json&units=metric&lang=zh_cn&APPID=6a67ed641c0fda8b69715c43518b6996'
+    contents=r.urlopen(url).read().decode('utf-8')
+    contents=json.loads(contents)
+
+    now = datetime.now()
+
+    current_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+
+    # json data
+    data = {
+      "timestamp" : current_time,
+      "value": contents
+    }
+
+    # send the contents under topic utilizations. Note that it expects
+    # the contents in bytes so we convert it to bytes.
+    #
+    # Note that here I am not serializing the contents into JSON or anything
+    # as such but just taking the output as received and sending it as bytes
+    # You will need to modify it to send a JSON structure, say something
+    # like <timestamp, contents of top>
+    #
+    producer.send ("utilizations", value=data)
+    producer.flush ()   # try to empty the sending buffer
+
+    # sleep a second
+    time.sleep (1)
+
+# we are done
+producer.close ()
---
+++ PA1/1/README.md
@@ -0,0 +1,75 @@
+# Team7 PA1 README
+
+# Milestone 1
+
+# Import .pem file to enable VM access:
+# Chameleon Cloud:
+
+chmod 600 Team7_Chameleon_SSH.pem
+ssh-add Team7_Chameleon_SSH.pem
+
+# AWS:
+
+chmod 600 Team7_AWS_SSH.pem
+ssh-add Team7_AWS_SSH.pem
+
+# ssh into the instance: 
+
+# VM 2 & 3 (Chameleon)
+ssh cc@129.114.25.15 
+ssh cc@129.114.25.58
+
+# VM 2 & 3 (AWS)
+ssh ubuntu@54.242.9.157 
+ssh ubuntu@54.242.145.238
+
+# Milestone 2
+
+# Start Zookeeper server and Kafka brokers
+
+# VM2 (Both Chameleon and AWS)：
+cd kafka_2.13-2.8.0
+bin/zookeeper-server-start.sh config/zookeeper.properties
+bin/kafka-server-start.sh config/server.properties
+
+# VM3 (Both Chameleon and AWS)：
+cd kafka_2.13-2.8.0
+bin/zookeeper-server-start.sh config/zookeeper.properties
+bin/kafka-server-start.sh config/server.properties --override broker.id=1
+
+# Open another terminal to execute producer.py and consumer.py
+
+# VM2 (Chameleon): 
+python3 consumer_cc.py
+
+# VM1.x (Local VM): 
+python3 producer_cc.py
+
+# Milestone 3
+
+# Execute producer.py and consumer.py
+
+# VM2 (Chameleon): 
+python3 consumer_cc.py
+
+# VM1.x (Local VM): 
+python3 producer_cc.py
+
+# VM2 (AWS): 
+python3 consumer_aws.py
+
+# VM1.x (Local VM): 
+python3 producer_aws.py
+
+# Check the data on the Fauxton web console
+
+# Chameleon Cloud:
+http://129.114.25.58:5984/_utils/#database/pa1/_all_docs
+
+# AWS:
+http://54.242.145.238:5984/_utils/#database/pa1/_all_docs
+
+==================================
+Documentations:
+1. API data: http://api.openweathermap.org/data/2.5/weather?q=chongqing&mode=json&units=metric&lang=zh_cn&APPID=6a67ed641c0fda8b69715c43518b6996
+Source: Openweathermap
